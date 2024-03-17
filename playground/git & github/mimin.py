c5108fa568713c042c75b20e3d7105e3d45f0edc
Avrisadh23 <147987603+Avrisadh23@users.noreply.github.com>
2024-03-17 14:05:02 +0700

    Update mimin.py

--- playground/git & github/mimin.py
+++ playground/git & github/mimin.py
@@ -1,4 +1,4 @@
 print(''' 
       Halo namaku admin GWE 2024. Aku suka ngoding kalau terpaksa. Saat ini aku lagi belajar bahasa Python terutama buat pengolahan data dan tertarik di bidang Artificial Intelligence. Salam kenal ya!
       ''')
-print(" Namaku muhamad Avrisad Garin Rahaguna")
+print("Halo min kenalin juga Namaku muhamad Avrisad Garin Rahaguna")
